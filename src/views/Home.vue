e0577e744389079fb4e5229075afd82b80a588e2
Issa <chrisissifou@gmail.com>
2021-12-22 22:34:26 +0100

    ref and reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div ref="p">  Mon nom est {{ firstname }} {{ lastname }}</div>
+  
+    <p>{{ fullname }} </p>
+    <p>{{ age }} </p>
+
+    <div><input type="text" v-model="firstname"></div>
+    
+    <button @click="add">Add</button>
+
+    <button @click="age++">Add</button>
+
+    <button @click="addDate">addDate</button>
+
+       <p>{{ name_and_age.name }} </p>
+        <p>{{ name_and_age.date }} </p>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+  import { ref } from "vue";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+
+  setup() {
+    
+    const firstname = ref("Issa")
+    const lastname = ref("Issifou")
+    const age = ref(88)
+
+    const name_and_age = ref({ name : 'Ali', date: 70 })
+
+    let fullname = ref('rrrrr')
+    const p = ref(null)
+
+    const add = () => {
+      
+       fullname.value = firstname.value + ' '  + lastname.value
+       p.value.classList.add("test")
+    }
+
+    const addDate = () => {
+      name_and_age.value.date = 50
+    }
+
+     return { firstname, lastname, add, fullname, p, age, name_and_age, addDate }
+  },
+
+ 
 }
 </script>
+
+<style scoped>
+.test {
+  color: red;
+  background: greenyellow;
+}
+</style>
