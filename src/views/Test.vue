ab10930829fe5913fc17c963655df0343590578f
Issifou Issa <chrisissifou@gmail.com>
2021-12-23 14:54:09 +0100

    watch and watchEffect

---
+++ src/views/Test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="test">
+        <h1>TEST</h1>
+        <p v-for="name in matchingNames" :key="name">{{ name }} </p>
+        <p>
+            <input type="text" v-model="search">
+        </p>
+        <p> {{ search }} </p>
+  </div>
+</template>
+
+<script>
+import { computed , ref, watch, watchEffect } from "vue";
+export default {
+    setup() {
+        const search = ref('')
+        const names = ref(["Toto", "Tata", "Titi", "Toutou", "Tutu", "Tete"])
+
+          watch(search, () => {
+            console.log('Watch function run')
+        })
+
+        const matchingNames = computed(() => {
+            // return names.value.filter(name => name.toLowerCase().indexOf(search.value.toLowerCase()) !== -1 )
+            return names.value.filter(name => name.toLowerCase().includes(search.value.toLowerCase()))
+        })
+
+        watchEffect(() => {
+            console.log('watchEffect function ran')
+        })
+            
+
+        return { names, search, matchingNames }
+    }
+}
+</script>
+
+<style>
+
+</style>
